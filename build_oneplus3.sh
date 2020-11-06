7fc2a4a76c6e2c36ee0bd89b6458b85ee316b151
MSe1969 <mse1969@posteo.de>
2020-11-06 22:13:42 +0100

    Build-script for oneplus3
    Change-Id: If05565db649791811c256b1455841e3f5ace8cb1

---
+++ build_oneplus3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Own values
+export CCACHE_DIR=~/.ccache17
+export OUT_DIR_COMMON_BASE=~/out-android
+
+# Use pre-defined build script
+source z_patches/build_device.sh oneplus3 $1
+
+
+
