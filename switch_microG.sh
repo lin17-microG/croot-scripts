9d9f438e5950786799c803ad55c2271d84f52450
MSe1969 <mse1969@posteo.de>
2020-11-26 23:17:01 +0100

    SWitch-script: Incorporate 'lin-17.1-hmalloc' branch
    Change-Id: I6e7e2fc87f9ab8e86b00cd187220f6b1069119f5

--- switch_microG.sh
+++ switch_microG.sh
@@ -25,25 +25,32 @@ switch_zpatch() {
 }
 
 case "$1" in
-  microG) 
+  microG)
     BRANCH1="lin-17.1-microG"
-#    BRANCH2="lineage-16.0"
-#    BRANCH3="lin-16.0-microG"
-#    BRANCH4="lin-16.0-microG"
+    BRANCH2="lineage-17.1"
+    BRANCH3="lin-17.1-microG"
+    BRANCH4="lin-17.1-microG"
     PATCHV="S"
     ;;
-  default) 
-    BRANCH1="lineage-17.1"
-#    BRANCH2="lineage-16.0"
-#    BRANCH3="lineage-16.0"
-#    BRANCH4="lineage-16.0"
+  hmalloc)
+    BRANCH1="lin-17.1-microG"
+    BRANCH2="lin-17.1-hmalloc"
+    BRANCH3="lin-17.1-hmalloc"
+    BRANCH4="lin-17.1-microG"
     PATCHV="S"
     ;;
-  reference) 
+  default)
     BRANCH1="lineage-17.1"
-#    BRANCH2="lineage-16.0"
-#    BRANCH3="lineage-16.0"
-#    BRANCH4="changelog"
+    BRANCH2="lineage-17.1"
+    BRANCH3="lineage-17.1"
+    BRANCH4="lineage-17.1"
+    PATCHV="S"
+    ;;
+  reference)
+    BRANCH1="lineage-17.1"
+    BRANCH2="lineage-17.1"
+    BRANCH3="lineage-17.1"
+    BRANCH4="changelog"
     PATCHV="N"
     ;;
   *) 
@@ -51,36 +58,26 @@ case "$1" in
     echo "-"
     echo "  default   - LineageOS 17.1"
     echo "  microG    - hardened microG build"
+    echo "  hmalloc   - hardened microG build with hardened-malloc"
     echo "  reference - 100% LineageOS 17.1 (no patches - for 'repo sync')"
     exit
     ;;   
 esac
 
-switch_zpatch $BRANCH1 R
+switch_zpatch $BRANCH3 R
 
 switch_branches $BRANCH1 art
-#switch_branches $BRANCH1 build/make
-#switch_branches $BRANCH2 external/selinux
-#switch_branches $BRANCH2 frameworks/av
+switch_branches $BRANCH2 bionic
+switch_branches $BRANCH1 build/make
 switch_branches $BRANCH1 frameworks/base
-#switch_branches $BRANCH1 frameworks/native
-#switch_branches $BRANCH2 frameworks/opt/net/wifi
-#switch_branches $BRANCH2 frameworks/opt/telephony
-#switch_branches $BRANCH1 lineage-sdk
+switch_branches $BRANCH3 frameworks/native
 switch_branches $BRANCH1 libcore
-#switch_branches $BRANCH3 packages/apps/Jelly
 switch_branches $BRANCH1 packages/apps/LineageParts
-switch_branches $BRANCH3 packages/apps/Nfc
+switch_branches $BRANCH1 packages/apps/Nfc
 switch_branches $BRANCH1 packages/apps/Settings
-#switch_branches $BRANCH3 packages/services/Telecomm
-#switch_branches $BRANCH2 packages/services/Telephony
-#switch_branches $BRANCH2 system/bt
-switch_branches $BRANCH1 system/core
-#switch_branches $BRANCH2 system/netd
-#switch_branches $BRANCH1 system/sepolicy
-#switch_branches $BRANCH2 system/vold
+switch_branches $BRANCH3 system/core
+switch_branches $BRANCH1 system/sepolicy
 switch_branches $BRANCH1 vendor/lineage
-#switch_branches $BRANCH2 vendor/qcom/opensource/cryptfs_hw
 switch_branches $BRANCH1 .repo/local_manifests
 #switch_branches $BRANCH4 OTA
 
